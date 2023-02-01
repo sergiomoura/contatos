a6b84d9b907040914b48f34f297e44fcbeca7c3b
Sérgio Moura <smouracalmon@gmail.com>
2023-02-01 02:00:21 -0300

    test: externalize consts in webapp specs

--- tests/types/WebApp.spec.ts
+++ tests/types/WebApp.spec.ts
@@ -5,49 +5,44 @@ import { type Request } from '@/types/Request';
 import { type Router } from '@/types/Router';
 import { describe, expect, test } from 'vitest';
 
+const host = 'http://localhost';
+const port = 3030;
+const responseBody = { msg: 'test' };
+const responseStatus = 200;
+const response = { status: responseStatus, body: responseBody };
+const controller = <Controller>(<unknown>{ handle: (req: Request) => { return response; } });
+const path1 = '/';
+const path2 = '/test';
+
+const app = settings.webApp;
+const router: Router = {
+  basePath: '',
+  middlewares: [],
+  routes: [
+    {
+      method: HttpMethod.GET,
+      path: path1,
+      controller,
+      middlewares: []
+    },
+    {
+      method: HttpMethod.GET,
+      path: path2,
+      controller,
+      middlewares: []
+    }
+  ]
+};
+app.setRouter(router);
+app.listen(port);
+
 describe(
   'WebApp specifications',
   
   () => {
 
-    const host = 'http://localhost';
-    const port = 3030;
-    const responseBody = { msg: 'test' };
-    const responseStatus = 200;
-    const response = { status: responseStatus, body: responseBody };
-
     test('Should call first level controllers',
       async () => {
-
-        const controller1: Controller = <Controller>(<unknown>{ handle: (req: Request) => { return response; } });
-        const controller2: Controller = <Controller>(<unknown>{ handle: (req: Request) => { return response; } });
-
-        const path1 = '/';
-        const path2 = '/test';
-
-        const app = settings.webApp;
-
-        const router: Router = {
-          basePath: '',
-          middlewares: [],
-          routes: [
-            {
-              method: HttpMethod.GET,
-              path: path1,
-              controller: controller1,
-              middlewares: []
-            },
-            {
-              method: HttpMethod.GET,
-              path: path2,
-              controller: controller2,
-              middlewares: []
-            }
-          ]
-        };
-        
-        app.setRouter(router);
-        app.listen(port);
 
         let res = await fetch(`${host}:${port}${path1}`);
         let content = await res.json();
