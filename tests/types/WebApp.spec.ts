37540d75f04e8dedb1248f95daa92217449435f0
Sérgio Moura <smouracalmon@gmail.com>
2023-02-05 23:36:16 -0300

    test: define webapp specifications

--- tests/types/WebApp.spec.ts
+++ tests/types/WebApp.spec.ts
@@ -2,7 +2,7 @@ import { settings } from '@/settings';
 import { type Controller } from '@/types/Controller';
 import { HttpMethod } from '@/types/HttpMethod';
 import { type Request } from '@/types/Request';
-import { type Router } from '@/types/Router';
+import { type Route } from '@/types/Route';
 import { describe, expect, test } from 'vitest';
 
 const host = 'http://localhost';
@@ -11,7 +11,7 @@ const responseBody = { msg: 'test' };
 const responseStatus = 200;
 const response = { status: responseStatus, body: responseBody };
 const controller = <Controller>(<unknown>{ handle: (req: Request) => { return response; } });
-const path1 = '/';
+const path1 = '';
 const path2 = '/test';
 const sub = '/sub';
 const subA = '/sub-a';
@@ -19,55 +19,62 @@ const subB = '/sub-b';
 const subB1 = '/sub-b-1';
 const subB2 = '/sub-b-2';
 const app = settings.webApp;
-const router: Router = {
-  basePath: '',
-  middlewares: [],
-  routes: [
-    {
-      method: HttpMethod.GET,
-      path: path1,
-      controller,
-      middlewares: []
-    },
-    {
-      method: HttpMethod.GET,
-      path: path2,
-      controller,
-      middlewares: []
-    },
-    {
-      basePath: sub,
-      routes: [
-        {
-          controller,
-          path: subA,
-          method: HttpMethod.GET
+const routes: Route[] = [
+  {
+    path: path1,
+    middlewares: [],
+    handler: { method: HttpMethod.GET, controller },
+    children: [
+      {
+        path: path2,
+        handler: {
+          method: HttpMethod.GET,
+          controller
         },
-        {
-          controller,
-          path: subB,
-          method: HttpMethod.GET
-        },
-        {
-          basePath: subB,
-          routes: [
-            {
+        middlewares: []
+      },
+      {
+        path: sub,
+        handler: { method: HttpMethod.GET, controller },
+        children: [
+          {
+            path: subA,
+            handler: {
               controller,
-              path: subB1,
-              method: HttpMethod.GET
-            },
-            {
-              controller,
-              path: subB2,
               method: HttpMethod.GET
             }
-          ]
-        }
-      ]
-    }
-  ]
-};
-app.setRouter(router);
+          },
+          {
+            path: subB,
+            handler: {
+              controller,
+              method: HttpMethod.GET
+            },
+            children: [
+              {
+                path: subB1,
+                handler: {
+                  controller,
+                  method: HttpMethod.GET
+                }
+              },
+              {
+                path: subB2,
+                handler: {
+                  controller,
+                  method: HttpMethod.GET
+                }
+              }
+            ]
+            
+          }
+        ]
+      }
+    ]
+  }
+];
+
+app.setRoutes(routes, '');
 app.listen(port);
 
 describe(
@@ -77,11 +84,13 @@ describe(
 
     test('Should call level 1 controllers',
       async () => {
-
+        
+        console.log(`Required: ${host}:${port}${path1}`);
         const response1 = await fetch(`${host}:${port}${path1}`);
         expect(response1.status).toBe(200);
         expect(await response1.json()).toEqual(responseBody);
-
+        
+        console.log(`Required: ${host}:${port}${path2}`);
         const response2 = await fetch(`${host}:${port}${path2}`);
         expect(response2.status).toBe(200);
         expect(await response2.json()).toEqual(responseBody);
@@ -89,33 +98,33 @@ describe(
       }
     );
   
-    test('Should call level 2 controllers',
-      async () => {
+    // test('Should call level 2 controllers',
+    //   async () => {
 
-        const response1 = await fetch(`${host}:${port}${sub}${subA}`);
-        expect(response1.status).toBe(200);
-        expect(await response1.json()).toEqual(responseBody);
+    //     const response1 = await fetch(`${host}:${port}${sub}${subA}`);
+    //     expect(response1.status).toBe(200);
+    //     expect(await response1.json()).toEqual(responseBody);
 
-        const response2 = await fetch(`${host}:${port}${sub}${subB}`);
-        expect(response2.status).toBe(200);
-        expect(await response2.json()).toEqual(responseBody);
+    //     const response2 = await fetch(`${host}:${port}${sub}${subB}`);
+    //     expect(response2.status).toBe(200);
+    //     expect(await response2.json()).toEqual(responseBody);
       
-      }
-    );
+    //   }
+    // );
 
-    test('Should call level 3 controllers',
-      async () => {
+    // test('Should call level 3 controllers',
+    //   async () => {
 
-        const response1 = await fetch(`${host}:${port}${sub}${subB}${subB1}`);
-        expect(response1.status).toBe(200);
-        expect(await response1.json()).toEqual(responseBody);
+    //     const response1 = await fetch(`${host}:${port}${sub}${subB}${subB1}`);
+    //     expect(response1.status).toBe(200);
+    //     expect(await response1.json()).toEqual(responseBody);
 
-        const response2 = await fetch(`${host}:${port}${sub}${subB}${subB2}`);
-        expect(response2.status).toBe(200);
-        expect(await response2.json()).toEqual(responseBody);
+    //     const response2 = await fetch(`${host}:${port}${sub}${subB}${subB2}`);
+    //     expect(response2.status).toBe(200);
+    //     expect(await response2.json()).toEqual(responseBody);
       
-      }
-    );
+    //   }
+    // );
     
   }
 
