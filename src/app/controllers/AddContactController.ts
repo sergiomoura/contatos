4646c2d9b468ec04289605f5b372980615305996
Sérgio Moura <smouracalmon@gmail.com>
2023-03-30 16:12:20 -0300

    refactor: make controller independent from entity

--- src/app/controllers/AddContactController.ts
+++ src/app/controllers/AddContactController.ts
@@ -1,9 +1,6 @@
 import { type AuthenticatedRequest } from '@/types/AuthenticatedRequest';
 import { type Controller } from '@/types/Controller';
 import { type Response } from '@/types/Response';
-import { Contact } from '../entities/Contact';
-import { Email } from '../entities/Email';
-import { PhoneNumber } from '../entities/PhoneNumber';
 import { type AddContactUseCase } from '../usecases/AddContactUseCase';
 
 export class AddContactController implements Controller {
@@ -12,11 +9,10 @@ export class AddContactController implements Controller {
   async handle (request: AuthenticatedRequest): Promise<Response> {
 
     const name = (<{ name: string }>request.body).name;
-    const emails = (<{ emails: string[] }>request.body).emails.map(Email.create);
-    const phoneNumbers = (<{ phoneNumbers: string[] }>request.body).phoneNumbers.map(PhoneNumber.create);
+    const emails = (<{ emails: string[] }>request.body).emails;
+    const phoneNumbers = (<{ phoneNumbers: string[] }>request.body).phoneNumbers;
 
-    const contact = Contact.create(name, emails, phoneNumbers);
-    await this.addContactsUseCase.execute(request.user.id, contact);
+    const contact = await this.addContactsUseCase.execute(request.user.id, name, emails, phoneNumbers);
     
     return <Response>{
       status: 200,
--- src/app/usecases/AddContactUseCase.ts
+++ src/app/usecases/AddContactUseCase.ts
@@ -1,12 +1,18 @@
-import { type Contact } from '../entities/Contact';
+import { Contact } from '../entities/Contact';
+import { Email } from '../entities/Email';
+import { PhoneNumber } from '../entities/PhoneNumber';
 import { type Repository } from '../repositories/Repository';
 
 export class AddContactUseCase {
 
   constructor (private readonly repository: Repository) {}
-  async execute (userId: string, contact: Contact): Promise<void> {
-   
+  async execute (userId: string, name: string, emailStrings: string[], phoneNumberStrings: string[]): Promise<Contact> {
+    
+    const emails = (emailStrings.map(Email.create));
+    const phoneNumbers = phoneNumberStrings.map(PhoneNumber.create);
+    const contact = Contact.create(name, emails, phoneNumbers);
     await this.repository.addContactToUser(userId, contact);
+    return contact;
   
   }
 
