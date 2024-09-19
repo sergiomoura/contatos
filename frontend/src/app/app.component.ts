f5c7c186e42748bcbe8bb1b3667d4a60bdf11fe2
Sérgio Siqueira <sergiosiqueira@movida.com.br>
2024-09-19 08:13:03 -0300

    chore: test fron back communication

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HttpClient } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, HttpClientModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private http: HttpClient) {
+    http.get("http://localhost:5000/api/v1/docs").subscribe((res)=>{console.log(res)});
+    console.log("teste") 
+  }
 }
